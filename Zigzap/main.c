edd8354a79d2b9f016ff4494292f8cba094809e6
Terry <bthvi@outlook.com>
2017-03-04 00:43:13 +0800

    Initial Commit

---
+++ Zigzap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Zigzap
+//
+//  Created by 雷琪 on 2017/3/4.
+//  Copyright © 2017年 雷琪. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+      // insert code here...
+      printf("Hello, World!\n");
+    return 0;
+}
