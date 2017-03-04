612f9d30fc397bcabbf009a440f6d1f309537695
Terry <bthvi@outlook.com>
2017-03-04 13:01:37 +0800

    zigzag

--- Zigzap/main.c
+++ Zigzap/main.c
@@ -7,9 +7,32 @@
 //
 
 #include <stdio.h>
+#include "stdlib.h"
+char* convert(char* s, int numRows) {
+      long len = strlen(s);
+      char *a =(char*)malloc(len+1);
+      int i=0,j=0,k=0;
+      if (numRows < 2) {
+            return s;
+      }
+      for (i =0; i<numRows; i++) {
+            for (j=i;j<len; j=j+2*(numRows-1)) {
+                  a[k++] = s[j];
+                  if (i>0&&i<numRows-1){
+                        if (j+2*(numRows-1-i)<len) {
+                              a[k++] = s[j+2*(numRows-1-i)];
+                        }
+                  }
+            }
+      }
+      if (k==len) {
+            a[k] = '\0';
+      }
+      return a;
+}
 
-int main(int argc, const char * argv[]) {
-      // insert code here...
-      printf("Hello, World!\n");
-    return 0;
+int main(){
+      char *ptr = convert("Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.", 2);
+      printf("%s\n",ptr);
+      return  0;
 }
